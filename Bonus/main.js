0ce498494add999844966be211f40b66a95121e1
Alessio2021 <alessiobut1911@gmail.com>
2021-11-22 17:47:58 +0100

    font color

--- Bonus/main.js
+++ Bonus/main.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const active = document.querySelector(".header-right a i");
 const notActive = document.querySelector(".close");
-const text = document.querySelector("ul li a")
+const text = document.querySelector(".hamburger-menu ul li a")
 
 active.addEventListener("click", function () {
     hamburger.classList.add("active");
     hamburger.style.backgroundColor = "grey";
-    text.classList.add("orangered");
+    text.style.color = "orange";
 });
 
 notActive.addEventListener("click", function () {
