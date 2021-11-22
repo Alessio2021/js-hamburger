af90ca3ef3195618aa4c521dbd9117a84a4d6c1a
Alessio2021 <alessiobut1911@gmail.com>
2021-11-22 15:19:45 +0100

    update no bootstrap

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector(".hamburger-menu");
-const active = document.querySelector(".fa-bars");
-const notActive = document.querySelector(".fa-times");
+const active = document.querySelector(".header-right a i");
+const notActive = document.querySelector(".close");
 
 active.addEventListener("click", function () {
     hamburger.classList.add("active");
